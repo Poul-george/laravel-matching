3518ef0cb1f59500568b66712b03f7498a1f0ecc
Poul-george <65060101+Poul-george@users.noreply.github.com>
2022-07-22 22:05:43 +0900

    Delete resources/views/client directory

--- resources/views/client/component/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div class="footer_content">
-        <a href="/content/privacy_policy" target="_blank">プライバシーポリシー</a>
-        <br>
-        <a href="/content/service_terms" target="_blank">利用規約</a>
-</footer>
